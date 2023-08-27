bfea11a99ed5408a5b72a1f999f47d6f290a27d0
emrah-i <ibraem1026@gmail.com>
2023-08-27 12:36:04 -0400

    Update

--- project4/settings.py
+++ project4/settings.py
@@ -128,6 +128,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/network/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/Network/network/static/staticfiles')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/network/static')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/Network/network/static/staticfiles/images')
